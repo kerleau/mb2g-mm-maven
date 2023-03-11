d5e72a50c6cbe6d556d290d00e835ac74b722f0d
kerleau <37364328+kerleau@users.noreply.github.com>
2023-03-11 15:07:00 +0000

    add enforcer rules to pom

---
+++ converters/target/generated-sources/annotations/alex/springframework/mappers/UserMapperImpl.java
@@ -0,0 +1,39 @@
+package alex.springframework.mappers;
+
+import alex.springframework.domain.UserCommand;
+import alex.springframework.entities.User;
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-03-11T15:01:19+0000",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.6 (Microsoft)"
+)
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public UserCommand userToUserCommand(User user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        UserCommand userCommand = new UserCommand();
+
+        userCommand.setEmail( user.getEmail() );
+
+        return userCommand;
+    }
+
+    @Override
+    public User userCommandToUser(UserCommand userCommand) {
+        if ( userCommand == null ) {
+            return null;
+        }
+
+        User.UserBuilder user = User.builder();
+
+        user.email( userCommand.getEmail() );
+
+        return user.build();
+    }
+}
