904f6a3ebc62077802bf91cefa5db359c3d33a4a
kerleau <37364328+kerleau@users.noreply.github.com>
2023-03-11 14:43:39 +0000

    add flattened poms files to ignore

--- converters/target/generated-sources/annotations/alex/springframework/mappers/UserMapperImpl.java
+++ converters/target/generated-sources/annotations/alex/springframework/mappers/UserMapperImpl.java
@@ -6,7 +6,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-03-11T13:54:17+0000",
+    date = "2023-03-11T14:13:16+0000",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.6 (Microsoft)"
 )
 public class UserMapperImpl implements UserMapper {
